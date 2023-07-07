27805706fb285b7397f6f91f5ff194ca31a8d878
Keshav Biswa <keshavbiswa21@gmail.com>
2023-07-07 20:16:42 +0530

    added global puts stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ SimpleCov.start do
 end
 
 RSpec.configure do |config|
+
+  config.before do
+    allow($stdout).to receive(:puts) unless ENV["DEBUG"] == "true"
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
