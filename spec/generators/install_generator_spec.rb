d422bb7c5891d26ba479a38a5270d5ba0046f9ac
Keshav Biswa <keshavbiswa21@gmail.com>
2023-07-21 21:35:47 +0530

    added more tests

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -3,45 +3,70 @@ require "generators/seedie/install_generator"
 require "fileutils"
 require "generator_spec"
 require "rails_helper"
+require "seedie"
 
 RSpec.describe Seedie::Generators::InstallGenerator, type: :generator do
   destination File.expand_path("../../tmp", __FILE__)
+  let(:seedie_config) { File.join(destination_root, "config", "seedie.yml") }
+  let(:content) { YAML.load_file(seedie_config) }
 
   before do
     Rails.application.eager_load!
     prepare_destination
+    run_generator
   end
+
 
   after do
-    FileUtils.rm_rf destination_root
+    # FileUtils.rm_rf destination_root
   end
 
-  context "when a seedie.yml file doesn't exist" do
-    it "creates a config file" do
-      run_generator
-      expect(destination_root).to have_structure {
-        directory "config" do
-          file "seedie.yml" do
-            contains "default_count: 5"
-            contains "models:"
-            contains "user:" do
-              contains "count: 10"
-              contains "attributes:" do
-                contains "name: name {{index}}"
-                contains "email: email {{index}}"
-              end
-              contains "disabled_fields: []"
-            end
-            contains "post:" do
-              contains "count: 10"
-              contains "attributes:" do
-                contains "title: title {{index}}"
-              end
-              contains "disabled_fields: []"
-            end
-          end
-        end
-      }
+  it "creates a config file" do
+    expect(File).to exist(seedie_config)
+  end
+
+  describe "#model_configuration" do
+
+    it "excludes reserved models" do
+      excluded_models = described_class::EXCLUDED_MODELS.map(&:underscore)
+      expect(content["models"]).not_to include(*excluded_models)
+    end
+
+    it "excludes abstract models" do
+      expect(content["models"]).not_to include("application_record")
+    end
+
+    it "excludes models without tables" do
+      expect(content["models"]).not_to include("model_without_table")
+    end
+
+    it "excludes HABTM models" do
+      expect(content["models"]).not_to include("habtm_game_rooms", "habtm_users")
+    end
+
+    it "generates required models" do
+      expect(content["models"]).to include("user", "post", "comment", "game_room")
+    end
+
+    it "sorts models by dependency" do
+      expect(content["models"].keys).to eq ["user", "post", "post_metadatum", "game_room", "comment"]
+    end
+
+    it "generates model_configuration for each model" do
+      expect(content["models"].each { |model, config| expect(config).to include("attributes", "associations") })
+    end
+  end
+
+  describe "#columns_configuration" do
+    it "generates required attributes" do
+      expect(content["models"]["user"]["attributes"]).to include("name", "email", "address", "nickname", "password")
+      expect(content["models"]["post"]["attributes"]).to include("title", "content")
+      expect(content["models"]["comment"]["attributes"]).to include("content")
+      expect(content["models"]["game_room"]["attributes"]).to include("name")
+    end
+
+    it "generates required belongs_to associations" do
+      expect(content["models"]["comment"]["associations"]["belongs_to"]).to include("post")
     end
   end
 end
