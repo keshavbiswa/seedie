9427d01caaa8711d76733c7bcbb602461a9a7f92
Keshav Biswa <keshavbiswa21@gmail.com>
2023-07-21 20:00:12 +0530

    Foreign keys

---
+++ spec/integration/foreign_key_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "ForeignKey" do
+  let(:config_path) { "spec/fixtures/foreign_key_config.yml" }
+
+  describe "seeding the Game Model" do
+    before do
+      Seedie::Seeder.new(config_path).seed_models
+    end
+
+    it "seeds the GameRoom model based on the given config" do
+      expect(GameRoom.count).to eq 5
+      expect(GameRoom.first.name).to eq "Game Room 0"
+      expect(GameRoom.first.creator_id).to be_in(User.ids)
+      expect(GameRoom.first.updater_id).to be_in(User.ids)
+    end
+  end
+end
